f4513c3de7997da3ee11189dd436f3f16e728059
Victor Williams <vrwilliams719@gmail.com>
2020-02-03 02:06:14 -0500

    dark

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,13 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
+DarkReader.enable({
+    brightness: 100,
+    contrast: 90,
+    sepia: 10
+});
+
+DarkReader.disable();
 
 (function($) {
 
