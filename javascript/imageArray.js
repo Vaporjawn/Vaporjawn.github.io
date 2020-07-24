c65b5f0292727509e986b5ede1bf7850d101ba31
Victor Williams <vrwilliams719@gmail.com>
2020-07-23 21:33:04 -0400

    add 3rd section

--- javascript/imageArray.js
+++
@@ -1,3 +0,0 @@
-let random_images_array = ["1.jpg", ]
-
-//random image selector
---
+++ javascript/javascript.js
@@ -0,0 +1,13 @@
+function selectPicture() {
+    var pictureNumber = "";
+    pictureNumber = Math.floor(Math.random() * 210)+1;
+    pictureNumber = pictureNumber + ".jpg"
+     return pictureNumber;
+    }
+//random image selector
+function displayPicture(){
+document.getElementById("img1").src = "./instagram_pictures/" + selectPicture();
+document.getElementById("img2").src = "./instagram_pictures/" + selectPicture();
+document.getElementById("img3").src = "./instagram_pictures/" + selectPicture();
+document.getElementById("img4").src = "./instagram_pictures/" + selectPicture();
+}
