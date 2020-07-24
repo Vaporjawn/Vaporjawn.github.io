a1c65b94ae1a2fa4e67ef1e21d482ac81b19ef7d
Victor Williams <vrwilliams719@gmail.com>
2020-07-24 18:42:02 -0400

    Update javascript.js

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -6,8 +6,8 @@ function selectPicture() {
     }
 //random image selector
 function displayPicture(){
-document.getElementById("img1").src = "https://github.com/Vaporjawn/Vaporjawn.github.io/blob/master/instagram_pictures/ + selectPicture();
-document.getElementById("img2").src = "https://github.com/Vaporjawn/Vaporjawn.github.io/blob/master/instagram_pictures/ + selectPicture();
-document.getElementById("img3").src = "https://github.com/Vaporjawn/Vaporjawn.github.io/blob/master/instagram_pictures/ + selectPicture();
-document.getElementById("img4").src = "https://github.com/Vaporjawn/Vaporjawn.github.io/blob/master/instagram_pictures/ + selectPicture();
+document.getElementById("img1").src = "https://github.com/Vaporjawn/Vaporjawn.github.io/blob/master/instagram_pictures/raw=true" + selectPicture();
+document.getElementById("img2").src = "https://github.com/Vaporjawn/Vaporjawn.github.io/blob/master/instagram_pictures/raw=true" + selectPicture();
+document.getElementById("img3").src = "https://github.com/Vaporjawn/Vaporjawn.github.io/blob/master/instagram_pictures/raw=true" + selectPicture();
+document.getElementById("img4").src = "https://github.com/Vaporjawn/Vaporjawn.github.io/blob/master/instagram_pictures/raw=true" + selectPicture();
 }setInterval(displayPicture,5000);
