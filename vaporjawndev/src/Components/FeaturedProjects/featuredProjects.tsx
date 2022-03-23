aa0d8e9f8c27f133de14f16c86538349f27c8d1b
Victor Williams <vrwilliams719@gmail.com>
2022-03-23 12:24:58 -0400

    Create featuredProjects.tsx

---
+++ vaporjawndev/src/Components/FeaturedProjects/featuredProjects.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+//Need to make object class for projects so that they have properties I can search for 
+//I could even use the Github api to get all of my projects from either the entire profile or a specific featured projects folder 
+
+function FeaturedProjects() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default FeaturedProjects;
