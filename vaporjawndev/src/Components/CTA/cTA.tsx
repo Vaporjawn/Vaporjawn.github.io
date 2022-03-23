5fadcff60458480a3171fd7728618ea6b0cc776f
Victor Williams <vrwilliams719@gmail.com>
2022-03-23 19:41:00 -0400

    Add CTA

---
+++ vaporjawndev/src/Components/CTA/cTA.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function CTA() {
+  return (
+    <div> 
+    <header>
+        <h2 className="glow"><strong> Want to reach me? </strong></h2>
+        <p className="glow"><b>Here's a few of my contacts and socials so we can be in touch.</b></p>
+    </header>
+    <footer>
+        <ul className="buttons">
+            <li><a href="mailto:victorwilliams719@gmail.com#" target="_blank" className="button special">Contact Me</a></li>
+            <li><a href="mailto:Vaporjawn@gmail.com#" target="_blank" className="button special">Vaporjawn Contact</a></li>
+        </ul>
+    </footer>
+    </div>
+  );
+}
+
+export default CTA;
