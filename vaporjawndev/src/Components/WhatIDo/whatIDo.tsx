d97a998040ef3a9339e231551e63d74baec79b82
Victor Williams <vrwilliams719@gmail.com>
2022-03-23 12:08:04 -0400

    Update whatIDo.tsx
    fixes

--- vaporjawndev/src/Components/WhatIDo/whatIDo.tsx
+++ vaporjawndev/src/Components/WhatIDo/whatIDo.tsx
@@ -6,14 +6,15 @@ function WhatIDo() {
             <header>
                 <h2><strong><b>a little bit about what i do</b></strong></h2>
             </header>
-            <p>I enjoy building webapps using <strong style="color: lightblue;">TypeScript</strong> with frameworks such as <strong style="color: #61DBFB"> React.JS </strong> or <strong style="color: #42b883">Vue.JS</strong> in the frontend, and
-                <strong>Node.js{/* style="color: darkgreen" */}</strong> <strong style="color: darkblue">.NET</strong>  or <strong style="color: Red;">Ruby</strong> in the backend. I've worked with <strong>MySQL, PostgreSQL, KSQL,
+            <p>I enjoy building webapps using <strong>TypeScript{/* style="color: lightblue;" */}</strong> with frameworks such as <strong> React.JS {/* style="color: #61DBFB" */}</strong> or <strong>Vue.JS{/* style="color: #42b883" */}</strong> in the frontend, and
+                <strong>Node.js{/* style="color: darkgreen" */}</strong> <strong>.NET{/* style="color: darkblue" */}</strong>  or <strong>Ruby{/* style="color: Red;" */}</strong> in the backend. I've worked with <strong>MySQL, PostgreSQL, KSQL,
                 & MongoDB</strong> in the database layer.  I've used both Azure and AWS relatively equally in terms of Cloud.  And I know my way around <strong>Xamarin</strong>, <strong>React Native</strong>, and <strong>Swift</strong>, 
                 and I commonly utilize <strong>RESTful APIs.</strong></p>
             <footer>
+            
                 <ul className="buttons">
-                    <li><a href="https://vaporjawn.github.io/Resume.html" target="_blank" class="special button">Check out my Resume</a></li>
-                    <li><a href="https://github.com/Vaporjawn/" target="_blank" class="special button">My Github</a></li>
+                    <li><a href="https://vaporjawn.github.io/Resume.html" target="_blank" className="special button">Check out my Resume</a></li>
+                    <li><a href="https://github.com/Vaporjawn/" target="_blank" className="special button">My Github</a></li>
                 </ul>
             </footer>
         </div>
