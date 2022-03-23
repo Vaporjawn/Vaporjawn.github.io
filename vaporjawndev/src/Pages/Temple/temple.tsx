7ee3f065858f8bbdd689f7b008c6bc831fc5886e
Victor Williams <vrwilliams719@gmail.com>
2022-03-23 19:38:36 -0400

    Update temple.tsx

--- vaporjawndev/src/Pages/Temple/temple.tsx
+++ vaporjawndev/src/Pages/Temple/temple.tsx
@@ -13,17 +13,17 @@ function Temple() {
         
         <h1 className="glow"><strong>The University</strong></h1>
         <h4>Temple University is a community of <strong>individuals who truly show up everyday to learn. </strong> It's one of the reasons that I was so interested in the field I'm in now. Everyone at Temple is there to help and I'm no better example of that.
-              I was able to blossom into a young adult with strong communication skills and got to diversify my knowledge with never-ending extracurricular activities.</h4>
+          I was able to blossom into a young adult with strong communication skills and got to diversify my knowledge with never-ending extracurricular activities.</h4>
         <p>Every year that I was at this University I felt like I was a completely different person.  This allowed me to get my feet wet into serveral different fields before even completing my 4-year degree. 
-            <br/> The school was everything that I could have ever wanted from <strong>'the college experince'.</strong> A truly diverse student body, along with dedicated professors to guide you into the real world as a professional in your major. 
-            <br/> They include a massive alumni network, while also providing an above average platform for employment through the University's career center. 
+          <br/> The school was everything that I could have ever wanted from <strong>'the college experince'.</strong> A truly diverse student body, along with dedicated professors to guide you into the real world as a professional in your major. 
+          <br/> They include a massive alumni network, while also providing an above average platform for employment through the University's career center. 
         </p>
         <header className="special container">
-            <span className="icon fa-moon-o"></span>
-            <h1 className="glow"><strong>The Students</strong></h1>
-            <h4>One of my favorite parts of attending Temple University was that so many of the students were dedicated about their eduacation and willingly shared knowledge between one another.
-                Everyone is there to learn and improve themselves.  It's mostly unspoken, yet respected. I've made countless lifetime friends here and hope to make many more. 
-            </h4>
+          <span className="icon fa-moon-o"></span>
+          <h1 className="glow"><strong>The Students</strong></h1>
+          <h4>One of my favorite parts of attending Temple University was that so many of the students were dedicated about their eduacation and willingly shared knowledge between one another.
+              Everyone is there to learn and improve themselves.  It's mostly unspoken, yet respected. I've made countless lifetime friends here and hope to make many more. 
+          </h4>
         </header>
 
                     {/* <ul class="buttons">
