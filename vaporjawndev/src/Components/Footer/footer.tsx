b6eed3a49d79188cb43aeaa79b71c1f37e87e62e
Victor Williams <vrwilliams719@gmail.com>
2022-03-23 23:30:48 -0400

    adding font awesome icons to footer

--- vaporjawndev/src/Components/Footer/footer.tsx
+++ vaporjawndev/src/Components/Footer/footer.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
+import './footer.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngellist, faFacebook, faGithub, faInstagram, faLinkedin, faSteam, faTwitch, faTwitter } from "@fortawesome/free-brands-svg-icons";
+
 
 function Footer() {
   return (
     <div>
-      <footer id="footer">
+      <footer className="footer">
         <ul className="icons">
-            <li><a href="https://github.com/vaporjawn" className="icon circle fa-github"><span className="label">Github</span></a></li>
-            <li><a href="https://twitter.com/vaporjawn" target="_blank" className="icon circle fa-twitter"><span className="label">Twitter</span></a></li>
-            <li><a href="https://Instagram.com/vaporjawn" target="_blank" className="icon circle fa-instagram"><span className="label">Instagram</span></a></li>
-            <li><a href="https://www.facebook.com/victor.williams.jr" target="_blank" className="icon brands circle fa-facebook"><span className="label">Facebook</span></a></li>
-            <li><a href="https://twitch.com/Vaporjawn" target="_blank" className="icon brands circle fa-twitch"><span className="label">Twitch</span></a></li>
-            <li><a href="https://steamcommunity.com/id/vaporjawn" target="_blank" className="icon brands circle fa-steam"><span className="label">Steam</span></a></li>
-            <li><a href="https://angel.co/victor-wiliams" target="_blank" className="icon brands circle fa-angellist"><span className="label">AngelLst</span></a></li>
-            <li><a href="https://linkedin.com/victorwilliams719" target="_blank" className="icon brands circle fa-linkedin"><span className="label">LinkedIn</span></a></li>
+          <li><a href="https://github.com/vaporjawn" target="_blank"><FontAwesomeIcon icon={faGithub}/></a></li>
+          <li><a href="https://twitter.com/vaporjawn" target="_blank"><FontAwesomeIcon icon={faTwitter}/></a></li>
+          <li><a href="https://instagram.com/vaporjawn" target="_blank"><FontAwesomeIcon icon={faInstagram}/></a></li>
+          <li><a href="https://www.facebook.com/victor.williams.jr" target="_blank"><FontAwesomeIcon icon={faFacebook}/></a></li>
+          <li><a href="https://twitch.com/vaporjawn" target="_blank"><FontAwesomeIcon icon={faTwitch}/></a></li>
+          <li><a href="https://steamcommunity.com/id/vaporjawn" target="_blank"><FontAwesomeIcon icon={faSteam}/></a></li>
+          <li><a href="https://angel.co/victor-wiliams" target="_blank"><FontAwesomeIcon icon={faAngellist}/></a></li>
+          <li><a href="https://linkedin.com/victorwilliams719" target="_blank"><FontAwesomeIcon icon={faLinkedin}/></a></li>
         </ul>
+        <br/>
         <ul className="copyright">
             <li>© Vaporjawn</li>
             <li>Design: <a href="http://github.com/vaporjawn/">Github</a></li>
