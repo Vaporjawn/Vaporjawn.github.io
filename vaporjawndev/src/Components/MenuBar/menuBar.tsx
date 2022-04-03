fd1a34d65c80a5e3f720e898d6d0661ded5d2318
Victor Williams <vrwilliams719@gmail.com>
2022-04-03 00:47:31 -0400

    start of menu bar

--- vaporjawndev/src/Components/MenuBar/menuBar.tsx
+++ vaporjawndev/src/Components/MenuBar/menuBar.tsx
@@ -7,7 +7,17 @@ import { faAngellist, faFacebook, faGithub, faInstagram, faLinkedin, faSteam, fa
 function MenuBar() {
   return (
     <div className='MenuBar'>
-      Place Holder for Menu
+      <ul className='menuList'>
+        <li className='menuOption'><a className='menuLink'>Victor's Resume</a></li>
+        <li className='menuOption'><a className='menuLink'>Vaporjawn</a></li>
+        <li className='menuOption'><a className='menuLink'>Temple</a></li>
+        <li className='menuOption'><a className='menuLink'>Home</a></li>
+        <li className='menuOption'><a className='menuLink'>Projects</a></li>
+        <li className='menuOption'><a className='menuLink'>Hackathons</a></li>
+        <li className='menuOption'><a className='menuLink'>SSBM</a></li>
+        <li className='menuOption'><a className='menuLink'>20XX</a></li>
+
+      </ul>
     </div>
   );
 }
