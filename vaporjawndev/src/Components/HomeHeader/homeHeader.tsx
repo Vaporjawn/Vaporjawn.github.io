0969fe0d1c78b8b74bee0d31edefdb40b35e167f
Victor Williams <vrwilliams719@gmail.com>
2022-04-02 20:17:02 -0400

    header css changes

--- vaporjawndev/src/Components/HomeHeader/homeHeader.tsx
+++ vaporjawndev/src/Components/HomeHeader/homeHeader.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './homeHeader.css';
 
 function HomeHeader() {
     return (
@@ -7,9 +8,8 @@ function HomeHeader() {
             <header>
                 <h2 className="glow">Victor Williams</h2>
             </header>
-            <p>A graduate of the beloved <strong><a href="./temple.html" target="_blank">Temple University.</a></strong> <br/> 
+            <p>A graduate of the beloved <strong ><a href="./temple.html" target="_blank" className='temple'>Temple University.</a></strong> <br/> 
                 I am a 24 year old <strong><b>Full Stack Developer</b></strong> living in the heart of<strong> Philadelphia</strong>.
-                {/* need to add style='color: #9D2235; to temple university */}
                 {/* need to change temple university link */}
             <br/></p>
             </div>
