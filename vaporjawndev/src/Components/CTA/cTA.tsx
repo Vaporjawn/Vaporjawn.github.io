1a52ffc9429f72f2bbd6500f61b11b0694c9855d
Victor Williams <vrwilliams719@gmail.com>
2022-04-03 01:22:03 -0400

    CTA buttons added

--- vaporjawndev/src/Components/CTA/cTA.tsx
+++ vaporjawndev/src/Components/CTA/cTA.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ButtonCTA from '../ButtonCTA/buttonCTA';
 import './cTA.css';
 
 function CTA() {
@@ -7,14 +8,8 @@ function CTA() {
     <header>
         <h2 className="CTAGlow"><strong> Want to reach me? </strong></h2>
         <p className="CTAGlow-small"><b>Here's a few of my contacts and socials so we can be in touch.</b></p>
+        <ButtonCTA/>
     </header>
-    <footer>
-        <ul className="buttons">
-            <li><a href="mailto:victorwilliams719@gmail.com#" target="_blank" className="button special">Contact Me</a></li>
-            <li><a href="mailto:Vaporjawn@gmail.com#" target="_blank" className="button special">Vaporjawn Contact</a></li>
-            {/* MAKE BOTH OF THESE BUTTON COMPONENTS */}
-        </ul>
-    </footer>
     </div>
   );
 }
