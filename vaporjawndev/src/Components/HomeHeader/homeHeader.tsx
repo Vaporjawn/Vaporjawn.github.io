88a1e11bcf1c36f02f1de97336bb4c039e0e6107
Victor Williams <vrwilliams719@gmail.com>
2022-04-12 22:57:21 -0400

    homeheader update

--- vaporjawndev/src/Components/HomeHeader/homeHeader.tsx
+++ vaporjawndev/src/Components/HomeHeader/homeHeader.tsx
@@ -7,7 +7,7 @@ function HomeHeader() {
             <div className="App-header">
                 <div>
                 <header>
-                    <h1 className="glow">VICTOR WILLIAMS</h1>
+                        <h1 className="glow">VICTOR WILLIAMS</h1>
                 </header>
                 <p className='blerb'>A graduate of the beloved <strong><a href="./temple.html" target="_blank" className='temple'>Temple University.</a></strong> <br/> 
                     I am a 24 year old <strong><b>Full Stack Developer</b></strong> living in the heart of<strong> Philadelphia</strong>.
