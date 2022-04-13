1f6c80a337f910b27da3e678a871af1afbdf1489
Victor Williams <vrwilliams719@gmail.com>
2022-04-13 15:14:54 -0400

    creating info box

--- vaporjawndev/src/Components/InfoBox/infoBox.tsx
+++ vaporjawndev/src/Components/InfoBox/infoBox.tsx
@@ -4,19 +4,19 @@ import './infoBox.css';
 function InfoBox() {
   return (
     <div>
-      <div className="wrapper style2 container special-alt">
-          <div className="row half">
-              <div className="8u">
+      <div className="wrapper-style2-container-special-alt">
+          <div className="row-half">
+              <div className="innerInfoBox">
                   <header>
-                      <h2><b><strong>a little bit about what i do</strong></b></h2></header>
-                  <p>I enjoy building webapps using <strong className='TypeScriptBlue'>TypeScript</strong> with frameworks such as <strong className="ReactBlue"> React.JS </strong> or <strong className="VueGreen">Vue.JS</strong> in the frontend, and
+                      <h2 className='infoBoxTitle'>a little bit about what i do</h2></header>
+                  <p className="infoBoxText">I enjoy building webapps using <strong className='TypeScriptBlue'>TypeScript</strong> with frameworks such as <strong className="ReactBlue"> React.JS </strong> or <strong className="VueGreen">Vue.JS</strong> in the frontend, and
                       <strong className="NodeJSGreen">Node.js</strong> <strong className="NETBlue">.NET</strong>  or <strong className='RubyRed'>Ruby</strong> in the backend. I've worked with <strong>MySQL, PostgreSQL, KSQL,
                       & MongoDB</strong> in the database layer.  I've used both Azure and AWS relatively equally in terms of Cloud.  And I know my way around <strong>Xamarin</strong>, <strong>React Native</strong>, and <strong>Swift</strong>, 
                       and I commonly utilize <strong>RESTful APIs.</strong></p>
                   <footer>
-                      <ul className="buttons">
-                          <li><a href="https://vaporjawn.github.io/Resume.html" target="_blank" className="special button">Check out my Resume</a></li>
-                          <li><a href="https://github.com/Vaporjawn/" target="_blank" className="special button">My Github</a></li>
+                      <ul className="infoBoxButtons">
+                          <li className='infoBoxPaddingLeft'><a href="https://vaporjawn.github.io/Resume.html" target="_blank" className="infoBoxButtonSpecial">Check out my Resume</a></li>
+                          <li className='infoBoxPaddingLeft'><a href="https://github.com/Vaporjawn/" target="_blank" className='infoBoxButtonSpecial'>My Github</a></li>
                       </ul>
                   </footer>
               </div>
