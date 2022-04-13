36593ae1261cd1bb18d704c57a670e0af7e575fe
Victor Williams <vrwilliams719@gmail.com>
2022-04-13 00:56:07 -0400

    adding some routes

--- vaporjawndev/src/Components/MenuBar/menuBar.tsx
+++ vaporjawndev/src/Components/MenuBar/menuBar.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 import './menuBar.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngellist, faFacebook, faGithub, faInstagram, faLinkedin, faSteam, faTwitch, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { Link } from 'react-router-dom';
 
 
 function MenuBar() {
   return (
     <div className='MenuBar'>
       <ul className='menuList'>
-        <li className='menuOption'><a className='menuLink'>Victor's Resume</a></li>
+        <li className='menuOption'><Link className='menuLink' to="/Resume">Victor's Resume</Link></li>
         <li className='menuOption'><a className='menuLink'>Vaporjawn</a></li>
-        <li className='menuOption'><a className='menuLink'>Temple</a></li>
+        <li className='menuOption'><Link className='menuLink' to={{pathname: "/Temple"}}>Temple</Link></li>
         <li className='menuOption'><a className='menuLink'>Home</a></li>
         <li className='menuOption'><a className='menuLink'>Projects</a></li>
         <li className='menuOption'><a className='menuLink'>Hackathons</a></li>
