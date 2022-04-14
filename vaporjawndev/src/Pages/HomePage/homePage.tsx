255b0c6c6f8ad1292171f4eb560d93eaf37d4423
Victor Williams <vrwilliams719@gmail.com>
2022-04-14 00:20:00 -0400

    start of the segmenter

--- vaporjawndev/src/Pages/HomePage/homePage.tsx
+++ vaporjawndev/src/Pages/HomePage/homePage.tsx
@@ -1,9 +1,11 @@
+import { faCode } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 import BackgroundBanner from '../../Components/BackgroundBanner/backgroundBanner';
 import BioSnippet from '../../Components/BioSnippet/bioSnippet';
 import CTABanner from '../../Components/CTABanner/cTABanner';
 import Footer from '../../Components/Footer/footer';
 import InfoBox from '../../Components/InfoBox/infoBox';
+import Segmenter from '../../Components/Segmenter/segmenter';
 // import logo from './logo.svg';
 // import './App.css';
 
@@ -18,6 +20,7 @@ function HomePage() {
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-48129201-1"></script>
       </head>
         <BackgroundBanner/>
+        <Segmenter iconName={faCode}/>
         <BioSnippet/>
         <InfoBox/>
         <CTABanner/>
