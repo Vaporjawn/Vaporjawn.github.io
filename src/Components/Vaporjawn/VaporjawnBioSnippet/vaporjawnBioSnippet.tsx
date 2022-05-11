1fb3241b44fa7f324c4e39f1cf702dd35dcacce0
Victor Williams <vrwilliams719@gmail.com>
2022-05-11 09:52:02 -0400

    VPR-102

--- src/Components/Vaporjawn/VaporjawnBioSnippet/vaporjawnBioSnippet.tsx
+++ src/Components/Vaporjawn/VaporjawnBioSnippet/vaporjawnBioSnippet.tsx
@@ -8,7 +8,7 @@ function VaporjawnBioSnippet() {
         <h4 className='large-font'><p>
           Vaporjawn was never intended to be as big as it became. The instagram account actually started out as a private instagram account where I would be able to post more personal content.
           I started out with a few posts here and there, but then I had encountered a big problem.  The camera on my iPhone 6S Plus, at the time, ceased to function at all; on both sides. 
-          At a crossroads, I was faced with either uploading screenshots that I had of pictures around the internet or creating my own instagram content entirely.  Obviously I chose 
+          <br/><br/>At a crossroads, I was faced with either uploading screenshots that I had of pictures around the internet or creating my own instagram content entirely.  Obviously I chose 
           the latter.  After making my initial vaporwave posts my followers influenced me to make my account public and it quickly grew along with requests and commissions.
         </p>
       </h4>
--- src/Components/Vaporjawn/VaporjawnHeader/vaporjawnHeader.tsx
+++ src/Components/Vaporjawn/VaporjawnHeader/vaporjawnHeader.tsx
@@ -6,9 +6,9 @@ function VaporjawnHeader() {
         <div>
             <div className="App-header">
         <header>
-            <h2 className="VaporjawnGlow">Vaporjawn</h2>
+            <h2 className="VaporjawnGlow">𝓽𝓱𝓮 𝓿𝓪𝓹𝓸𝓻𝔀𝓪𝓿𝓮 𝓵𝓲𝓯𝓮𝓼𝓽𝔂𝓵𝓮</h2>
         </header>
-        <p className='VaporjawnBlerb'>This is one of my favorite side project to talk about.<br/> <a className='VaporjawnLink' href="https://instagram.com/vaporjawn" target="_blank">Vaporjawn</a> 
+        <p className='VaporjawnBlerb'>Vaporjawn is a multi-medium collective inspired by the spectrum of art that exists in the vaporwave community. <a className='VaporjawnLink' href="https://instagram.com/vaporjawn" target="_blank">Vaporjawn </a> 
                     is my former "<a className='VaporjawnLink' href="https://www.dictionary.com/e/slang/finsta/#:~:text=A%20finsta%20is%20a%20secondary,have%20more%20heavily%20curated%20content." target="_blank">Finsta</a>"
                     that I turned business.</p>
             </div>
