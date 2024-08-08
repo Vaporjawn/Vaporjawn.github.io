951ef469aca8ff0703c8e9813ccd793d9f7152e0
Victor Williams <vrwilliams719@gmail.com>
2024-08-08 16:49:20 -0400

    fixing the lint process

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import prettier from 'eslint-plugin-prettier';
+import react from 'eslint-plugin-react'
 
 
 export default tseslint.config({
@@ -43,6 +44,7 @@ export default tseslint.config({
     'react-hooks': reactHooks,
     'react-refresh': reactRefresh,
     'prettier': prettier,
+    'react': react,
   },
   settings: {
     react: {
@@ -70,5 +72,14 @@ export default tseslint.config({
     'promise/always-return': 'off',
     'jsx-a11y/interactive-supports-focus': 'off',
     'quotes': ['error', 'double'],
+    'semi': ['error', 'always'],
+    "func-style": ["error", "declaration", { "allowArrowFunctions": true }],
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
   },
 })
